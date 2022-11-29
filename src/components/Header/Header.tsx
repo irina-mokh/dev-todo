67eba1a77cae06a61cca85f7dd1ddb8cf5de7ea6
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-11-29 18:56:57 +0300

    feat: change main color

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+import { Modal } from '../Modal/Modal';
+import { Search } from '../Search/Search';
+
 export const Header = () => {
+  const [isSearch, setIsSearch] = useState(false);
+
+  const handleClose = () => {
+    setIsSearch(false);
+  };
+
   return (
     <header className="header">
       <div className="container">
@@ -6,6 +16,19 @@ export const Header = () => {
           <a href="/" className="header__logo">
             Dev-todo
           </a>
+          <button
+            className="header__search"
+            onClick={() => {
+              setIsSearch(true);
+            }}
+          >
+            Search
+          </button>
+          {isSearch && (
+            <Modal title="Search" close={handleClose}>
+              <Search></Search>
+            </Modal>
+          )}
         </nav>
       </div>
     </header>
